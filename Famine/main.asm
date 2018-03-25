9f0587878ae95d6aba07b00eb9f4b57aae26f3e1
JeremShy <moc@yopmail.net>
2018-03-25 17:32:42 +0200

    Commit message super bien

--- Famine/main.asm
+++ Famine/main.asm
@@ -10,20 +10,48 @@ includelib libvcruntime.lib
 
 extrn puts:proc
 extrn exit:proc
+extrn _open:proc
+extrn printf:proc
 
 .data
 hello db 'Hello 64-bit world!',0ah,0
+print_decimal db 'open : %d',0ah,0
+
+; rbp
+; 40-48 : alignment
+; 32-40 : av[0]
+; 00-32 : shadow space
+; rsp 
 
 .code
+signature proc
+	db 'Famine version 1.0 (c)oded by magouin-jcamhi',0ah,0h
+signature endp
+
+find_start_famine proc
+
+find_start_famine endp
+
 main proc
 		push rbp
 		mov rbp, rsp
-		sub	rsp, 24
+		sub	rsp, 48
 
 		mov rcx, [rdx]
-		mov [rsp + 16], rcx
+
+		mov [rsp + 32], rcx
+
+		lea rcx, signature
 		call puts
 
+		mov rcx, [rsp + 32]
+		mov rdx, 8000h
+		mov	r8, 0
+		call _open
+
+		lea rcx, print_decimal
+		mov rdx, rax
+		call printf
 
 		mov rax, 0
 		mov rsp, rbp
